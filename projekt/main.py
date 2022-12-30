66f09db4b7f56180fccf0747e8f1c788037e9de5
Bartłomiej Rasztabiga <contact@rasztabiga.me>
2022-12-30 17:31:27 +0100

    WIP: NIE WIEM PO CO TO #6

--- projekt/main.py
+++ projekt/main.py
@@ -110,8 +110,7 @@ class BroadcastSendThread(StoppableThread):
         structs = []
         for file in files:
             filename = bytes(file.filename, "utf-8")
-            structs.append(struct.pack(f'!I{len(filename)}sddi?',
-                                       len(filename),
+            structs.append(struct.pack('!100sddi?',
                                        filename,
                                        file.created_at.timestamp(),
                                        file.modified_at.timestamp(),
@@ -199,9 +198,8 @@ class BroadcastListenThread(StoppableThread):
         structs = data.split(b";")
         files = []
         for strct in structs:
-            (i,), data = struct.unpack('!I', strct[:4]), strct[4:]
-            filename, created_at, modified_at, size, is_deleted = struct.unpack(f'!{i}sddi?', data)
-            files.append(File(filename.decode('utf-8'),
+            filename, created_at, modified_at, size, is_deleted = struct.unpack('!100sddi?', strct)
+            files.append(File(filename.decode('utf-8').trim(),
                               datetime.fromtimestamp(created_at),
                               datetime.fromtimestamp(modified_at),
                               size,
