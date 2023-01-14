1e89514dc4c068c82d9cb848a7f2420820e552cc
Bartłomiej Rasztabiga <contact@rasztabiga.me>
2023-01-14 15:20:41 +0100

    WIP: file system watcher v1

--- projekt/file_system/fs.py
+++ projekt/file_system/fs.py
@@ -5,24 +5,32 @@ from file_system.file import File
 
 
 def get_files_in_dir(path) -> list[File]:
-    filenames = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
     files = []
-    for filename in filenames:
-        stat = os.stat(os.path.join(path, filename))
-        files.append(File(
-            filename=filename,
-            created_at=datetime.fromtimestamp(stat.st_ctime),
-            modified_at=datetime.fromtimestamp(stat.st_mtime),
-            size=stat.st_size
-        ))
+
+    try:
+        filenames = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
+        for filename in filenames:
+            stat = os.stat(os.path.join(path, filename))
+            files.append(File(
+                filename=filename,
+                created_at=datetime.fromtimestamp(stat.st_ctime),
+                modified_at=datetime.fromtimestamp(stat.st_mtime),
+                size=stat.st_size
+            ))
+    except Exception as e:
+        print(e)  # TODO logging
+
     return files
 
 
 def save_file(path: str, file: File, content):
-    path = os.path.join(path, file.filename)
-    with open(path, 'wb') as f:
-        f.write(content)
-    os.utime(path, (file.modified_at.timestamp(), file.modified_at.timestamp()))
+    try:
+        path = os.path.join(path, file.filename)
+        with open(path, 'wb') as f:
+            f.write(content)
+        os.utime(path, (file.modified_at.timestamp(), file.modified_at.timestamp()))
+    except Exception as e:
+        print(e)  # TODO logging
 
 
 def read_file(path: str, filename: str) -> bytes | None:
@@ -35,4 +43,7 @@ def read_file(path: str, filename: str) -> bytes | None:
 
 
 def delete_file(path: str, filename: str):
-    os.remove(os.path.join(path, filename))
+    try:
+        os.remove(os.path.join(path, filename))
+    except Exception as e:
+        print(e)  # TODO logging
