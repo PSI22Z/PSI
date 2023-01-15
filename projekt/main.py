4df309ebac438821acb7cfd5e3638716f2b32ddc
Bartłomiej Rasztabiga <contact@rasztabiga.me>
2023-01-15 22:32:01 +0100

    WIP: error handling

--- projekt/main.py
+++ projekt/main.py
@@ -12,6 +12,26 @@ from threads.file_server_thread import FileServerThread
 from threads.file_sync_lock import file_sync_lock
 from utils.logger import get_logger
 
+threads = []
+logger = get_logger("FileSync")
+
+
+def stop():
+    file_sync_lock.release()
+
+    for t in threads:
+        t.stop()
+    for t in threads:
+        t.join()
+
+    logger.info('FileSync stopped')
+    sys.exit(0)
+
+
+def sigint_handler(sig, frame):
+    print("SIGINT")
+    stop()
+
 
 def main():
     if len(sys.argv) != 3:
@@ -25,7 +45,6 @@ def main():
 
     fs = FileSystem(syncing_path)
 
-    logger = get_logger("FileSync")
     logger.info('Starting FileSync')
 
     file_system_watcher_thread = FileSystemWatcherThread(fs)
@@ -33,10 +52,11 @@ def main():
     file_sync_client_thread = FileSyncClientThread(fs, network_interface)
     file_server_thread = FileServerThread(fs)
 
-    file_system_watcher_thread.start()
-    broadcast_send_thread.start()
-    file_sync_client_thread.start()
-    file_server_thread.start()
+    global threads
+    threads = [file_system_watcher_thread, broadcast_send_thread, file_sync_client_thread, file_server_thread]
+
+    for t in threads:
+        t.start()
 
     # TODO naprawic lub usunac struct, pickle jest w porządku ? a moze wlasny format
     # TODO dodac logging
@@ -50,21 +70,8 @@ def main():
             # could have used signal.pause() on linux, but it's not available on windows
             sleep(1)
         except KeyboardInterrupt:
-            file_sync_lock.release()
-
-            file_system_watcher_thread.stop()
-            broadcast_send_thread.stop()
-            file_sync_client_thread.stop()
-            file_server_thread.stop()
-
-            file_system_watcher_thread.join()
-            broadcast_send_thread.join()
-            file_sync_client_thread.join()
-            file_server_thread.join()
-
-            logger.info('FileSync stopped')
-
-            sys.exit(0)
+            print("KeyboardInterrupt")
+            stop()
 
 
 main()
