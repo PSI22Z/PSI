24e71bd40788bf2a7d7d13f9396ae3665483317b
Bartłomiej Rasztabiga <contact@rasztabiga.me>
2023-01-15 20:09:59 +0100

    WIP: get rid of pickle

--- projekt/network/deser.py
+++ projekt/network/deser.py
@@ -1,12 +1,30 @@
-import pickle
+import datetime
 from typing import List
 
 from file_system.file import File
+from utils.consts import ENCODING
 
 
 def serialize(files: List[File]) -> bytes:
-    return pickle.dumps(files)
+    msg = bytearray()
+    for file in files:
+        msg.extend(
+            f"{file.filename},{file.created_at.timestamp()},{file.modified_at.timestamp()},{file.size},{file.is_deleted};"
+            .encode(ENCODING))
+    print(msg)
+    return msg
 
 
 def deserialize(msg: bytes) -> List[File]:
-    return pickle.loads(msg)
+    files = []
+    for file in msg.decode(ENCODING).split(';')[:-1]:
+        filename, created_at, modified_at, size, is_deleted = file.split(',')
+        files.append(
+            File(filename,
+                 datetime.datetime.fromtimestamp(created_at),
+                 datetime.datetime.fromtimestamp(modified_at),
+                 size,
+                 is_deleted)
+        )
+    print(files)
+    return files
