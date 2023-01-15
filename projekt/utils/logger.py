5d11e30237de7ac39db4e43ea802b2e1502d1bb8
Bartłomiej Rasztabiga <contact@rasztabiga.me>
2023-01-15 19:38:22 +0100

    WIP: logging

--- projekt/utils/logger.py
+++ projekt/utils/logger.py
@@ -1,9 +1,13 @@
 import logging
+import sys
 
 
 def get_logger(logger_name):
-    logging.basicConfig(filename="filesync.log",
-                        filemode='a',
+    file_handler = logging.FileHandler('filesync.log', mode='a')
+    stdout_handler = logging.StreamHandler(sys.stdout)
+    handlers = [file_handler, stdout_handler]
+
+    logging.basicConfig(handlers=handlers,
                         format='%(asctime)s.%(msecs)-3d %(levelname)-8s %(name)-18s %(message)s',
                         datefmt='%Y-%m-%d %H:%M:%S',
                         level=logging.DEBUG)
