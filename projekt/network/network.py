2490cf993a2a0a42b742b9d85d84e0e0e84d1b52
Bartłomiej Rasztabiga <contact@rasztabiga.me>
2023-01-15 20:33:49 +0100

    WIP: cipher v3

--- projekt/network/network.py
+++ projekt/network/network.py
@@ -1,29 +1,39 @@
+from typing import Any, Tuple
+
+from network.cipher import xor
 from utils.consts import BUFF_SIZE, MAX_UDP_PACKET_SIZE
 
 
 # TODO add cipher/decipher
 
-def recv(conn):
-    return conn.recv(BUFF_SIZE)
+def recv(conn) -> bytes:
+    received = conn.recv(BUFF_SIZE)
+    # TODO handle exceptions
+    return xor(received)
 
 
-def recvfrom(sock):
-    return sock.recvfrom(MAX_UDP_PACKET_SIZE)
+def recvfrom(sock) -> Tuple[bytes, Any]:
+    received, addr = sock.recvfrom(MAX_UDP_PACKET_SIZE)
+    # TODO handle exceptions
+    return xor(received), addr
 
 
-def recvall(sock):
+def recvall(sock) -> bytes:
     data = bytearray()
     while True:
         part = sock.recv(BUFF_SIZE)
+        # TODO handle exceptions
         if not len(part):
             break
         data.extend(part)
-    return data
+    return xor(data)
 
 
-def sendto(sock, data, address):
-    sock.sendto(data, address)
+def sendto(sock, data: bytes, address):
+    sock.sendto(xor(data), address)
+    # TODO handle exceptions
 
 
-def sendall(conn, data):
-    conn.sendall(data)
+def sendall(conn, data: bytes):
+    conn.sendall(xor(data))
+    # TODO handle exceptions
